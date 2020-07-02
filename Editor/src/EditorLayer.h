6a6477591073d3e6eb26db1d3c5f8fb13648c81d
Zhang-dev-byte <ozhanghe@gmail.com>
2020-07-02 09:33:20 -0700

    Added Framebuffers

--- Editor/src/EditorLayer.h
+++ Editor/src/EditorLayer.h
@@ -1,5 +1,34 @@
 #pragma once
 #include <OSource.h>
+unsigned int FBO, TCB, RBO;
+namespace EditorGL {
+	extern void Genframebuffers() {
+		glGenFramebuffers(1, &FBO);
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+		glGenTextures(1, &TCB);
+		glBindTexture(GL_TEXTURE_2D, TCB);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1280, 720, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, TCB, 0);
+
+		glGenRenderbuffers(1, &RBO);
+		glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 1280, 720);
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			OS_CORE_ERROR("Framebuffer is not complete!");
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		OS_CORE_INFO(TCB);
+	}
+	extern void Bindframebuffer() {
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+	}
+	extern void Unbindframebuffer() {
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	}
+}
 class LayerEditor : public OSource::Layer {
 public:
 	virtual void OnRun() override {
@@ -37,6 +66,9 @@ public:
 			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
 			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
 		}
+		ImGui::Begin("Viewport");
+		ImGui::Image((void*)TCB, ImVec2(640, 360));
+		ImGui::End();
 		ImGui::End();
 	}
 	virtual void HandleInput(OSource::Input& input) override {
--- Editor/src/main.cpp
+++ Editor/src/main.cpp
@@ -6,10 +6,17 @@ public:
 	OSource::Camera cam = OSource::Camera(-1.6f, 1.6f, -0.9, 0.9);
 	OSource::Sprite s = OSource::Sprite(OSource::Texture("res/image.png"), "Hello");
 	virtual void Run() override{
+        EditorGL::Genframebuffers();
 		s.SetRotation(45);
 	}
 	virtual void Render() override{
+        s.SetRotation(90);
+        EditorGL::Bindframebuffer();
 		s.Render(cam);
+        EditorGL::Unbindframebuffer();
+        glClearColor(1, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
 	}
 	virtual void HandleInput(OSource::Input& input) override{
 	}
